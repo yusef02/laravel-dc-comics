64c7d5dc91e40806faf52a57ef5686393f6445e7
yusef02 <youssef.bouziane2402@gmail.com>
2024-03-31 13:28:01 +0200

    add Comic resource msrc and update Comic model

---
+++ app/Models/Comic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    use HasFactory;
+
+    public $table = 'comics';
+    protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
+}
