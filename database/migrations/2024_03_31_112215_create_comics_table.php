d9e93349980f9c90ef2250fd44c80c0a1728d315
yusef02 <youssef.bouziane2402@gmail.com>
2024-03-31 13:39:30 +0200

    add new create comics migration

--- database/migrations/2024_03_31_112215_create_comics_table.php
+++ database/migrations/2024_03_31_112215_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->double('price');
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->enum('type', ['comic book', 'graphic novel']);
+
             $table->timestamps();
         });
     }
