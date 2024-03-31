3167519f9bbcc093da02925b4bd225c58c095290
yusef02 <youssef.bouziane2402@gmail.com>
2024-03-31 16:13:40 +0200

    add comics.index view

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Comics index')
+
+@section('main-content')
+<div class="container py-5">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>TITLE</th>
+        <th>PRICE</th>
+        <th>SERIES</th>
+        <th>SALE DATE</th>
+        <th>TYPE</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      @forelse($comics as $comic)
+      <tr>
+        <td>{{ $comic['id'] }}</td>
+        <td>{{ $comic['title'] }}</td>
+        <td>{{ $comic['price'] }}</td>
+        <td>{{ $comic['series'] }}</td>
+        <td>{{ $comic['sale_date'] }}</td>
+        <td>{{ $comic['type'] }}</td>
+        <td>
+          <a href="{{ route('comics.show') }}">show</a>
+
+        </td>
+      </tr>
+      @empty
+      <tr>No comics found</tr>
+      @endforelse
+    </tbody>
+  </table>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComicController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-})->name('home');
+Route::get(
+  '/',
+  [PageController::class, 'index']
+)->name('home');
+
+// COMIC RESOURCE
+
+Route::resource('comics', ComicController::class);
+Route::resource('comics/{comic}', ComicController::class);
